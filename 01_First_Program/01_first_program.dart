599dc1c01d52a4acbbd5b944e82b72761e575dd5
Francois Snyman <francois.snyman@gmail.com>
2023-02-09 17:19:20 +0000

    Return args to user

--- 01_First_Program/01_first_program.dart
+++ 01_First_Program/01_first_program.dart
@@ -5,9 +5,14 @@ void printsrting(String input) {
   print('You entered: "' + input + '"');
 }
 
-void main() {
+void main(List<String> args) {
+  //terurn the arguments passed when running this program
+  print('Arguments passed when running this program:');
+  print(args);
+
   //prompt user to enter text and store it in a variable
   print('Please enter a string:');
+
   String? inputstring = stdin.readLineSync();
   //call the function
   printsrting(inputstring ?? '');
