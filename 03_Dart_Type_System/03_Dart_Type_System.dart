10d4240e9a7f29992d342b8f43d8b20aa27cf345
Francois Snyman <francois.snyman@gmail.com>
2023-02-15 11:36:24 +0000

    Complete Dart type system notes

---
+++ 03_Dart_Type_System/03_Dart_Type_System.dart
@@ -0,0 +1,31 @@
+// This is a Dart file where I tested the yype system in Dart.
+// ------------------- dynamic -------------------
+void main() {
+  dynamic name = 'Francois';
+  name = 42;
+
+  print(name.runtimeType); // Output: int
+
+  name = true;
+  print(name.runtimeType); // Output: bool
+}
+
+//------------------- const -------------------
+// void main() {
+//   var name = 'Francois';
+//   const upperName = name.toUpperCase(); // Error: This code will not tun because name.toUpperCase() is not known at compile time.
+
+//   print(name.runtimeType); // Output: String
+//   print(age.runtimeType); // Output: int
+// }
+
+// //------------------- Type Inference -------------------
+// void main() {
+//   String name = 'Francois';
+//   final nameUpper = name.toUpperCase();
+//   final int age = 42;
+
+//   print(name.runtimeType); // Output: String
+//   print(name.runtimeType); // Output: String
+//   print(age.runtimeType); // Output: int
+// }
